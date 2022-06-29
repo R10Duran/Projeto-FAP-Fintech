a05185861eccbe15f2936825259bbd5365a2d9f9
R10Duran <92634893+R10Duran@users.noreply.github.com>
2022-06-29 17:58:04 -0300

    primeiro commit

---
+++ JS/home.js
@@ -0,0 +1,16 @@
+/*data de saída inicio*/
+date = new Date()
+year = date.getFullYear()
+month = date.getMonth() + 1
+day = date.getDate()
+document.getElementById('data-saida').innerHTML = month + '/' + day + '/' + year
+/*data de saída fim*/
+
+/*data de entrada inicio*/
+date = new Date()
+year = date.getFullYear()
+month = date.getMonth() + 1
+day = date.getDate()
+document.getElementById('data-entrada').innerHTML =
+  month + '/' + day + '/' + year
+/*data de entrada fim*/
